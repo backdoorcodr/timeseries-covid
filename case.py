ad18a96aa658d664fcc21a805606b11ad37fad32
MuhammadSarimZafar <backdoorcoder@outlook.com>
2020-10-02 20:59:15 +0200

    create objects for each cases in data

---
+++ case.py
@@ -0,0 +1,11 @@
+class Case:
+    def __init__(self, country, confirmed_cases, deaths, recovered, update_time, region):
+        self.country = country
+        self.confirmed_cases = confirmed_cases
+        self.deaths = deaths
+        self.recovered = recovered
+        self.update_time = update_time
+        self.region = region
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import csv
 
-if __name__ == '__main__':
-    with open('/Users/szafar/PycharmProjects/timeseries-covid/COVID-19_geo_timeseries.csv', newline='') as csvfile:
+from case import Case
+
+cases_list = []
+def read_csv(file_path):
+    with open(file_path, newline='') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',')
         line_count = 0
         for row in csv_reader:
@@ -9,6 +12,13 @@ if __name__ == '__main__':
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
             else:
-                print(f'\t {row[1]} {row[4]} {row[5]} {row[6]} {row[7]} {row[10]}')
+                case = Case(row[1], row[4], row[5], row[6], row[7], row[10])
                 line_count += 1
-        print(f'Processed {line_count} lines.')
+                cases_list.append(case)
+    print(f'Processed {len(cases_list)} lines.')
+
+
+if __name__ == '__main__':
+    read_csv('/Users/szafar/PycharmProjects/timeseries-covid/COVID-19_geo_timeseries.csv')
+
+
